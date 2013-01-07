5d2e9904c22335fc89dc7f95efb515ac91a4a055
krishnaprasad.bv <krishnaprasad.bv@gmail.com>
2013-01-07 11:24:09 +0000

    Modified the query structure. Added the DB pointer and some definitions.

--- DBEngine/IndexQuery.h
+++ DBEngine/IndexQuery.h
@@ -1,3 +1,19 @@
+// For Drop Index
+// The Index pages has to be returned to the free list
+// Send the root page id
+// Noopur has to return all the pages from the root page to the free page list
+
+#include"DB.h"
+
+#define INDEXCREATE 1
+#define INDEXINSERT 2
+#define INDEXINSERTCHECK 3
+#define INDEXGET 4
+#define INDEXUPDATEKEY 5
+#define INDEXUPDATERECORD 6
+#define INDEXDELETEKEY 7
+#define INDEXDROP 8
+
 typedef struct recordID
 {
 	int dataPageID;
@@ -10,18 +26,30 @@ typedef struct indexQuery
 	int rootPageID;
 	char dataType[8];
 	int charLength;
-	bool insertFlag;
+
+	DB * curDB;
+
+	bool resultFlag;
+
 	char *key;
 	RecordID keyRecord;
+
 	int limitKeys;
 	int returnedKeys;
-	RecordID keyRecord[limitKeys];
+	int lastKeyID;
+	int totalResults;
+	RecordID **keyRecords;
+	
+	bool rootPageIDUpdated;
+	int newRootPageID;
+
 	char *oldKey;
 	int oldKeyLength;
 	char *newKey;
 	int newKeyLength;
 	RecordID oldKeyRecord;
 	RecordID newKeyRecord;
+
 	bool errorFlag;
 	int errorNum;
 }IndexQuery;
