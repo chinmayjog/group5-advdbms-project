eb78e337a865252ba24cd599ca0c38c8010af2a8
krishnaprasad.bv@gmail.com <krishnaprasad.bv@gmail.com@8937ff0b-c0a3-5707-dd86-2fc15fcea389>
2013-01-08 10:28:01 +0000

    Made small modifications. CreateDB in main.cpp file.

--- trunk/DBEngine/debug.sh
+++ trunk/DBEngine/debug.sh
@@ -6,5 +6,8 @@
 `g++ -c -g SysIndex.cpp`
 `g++ -c -g FreeList.cpp`
 `g++ -c -g DB.cpp`
-`g++ -c -g DBTest.cpp`
-`g++ -g SysTables.o SysColumns.o SysIndex.o FreeList.o DBHeader.o DB.o DBTest.o -o DBTestDebug.out`
+`g++ -c -g DataPage.cpp`
+`g++ -c -g DataTypes.cpp`
+`g++ -c -g dataDirectoryPage.cpp`
+`g++ -c -g main.cpp`
+`g++ -g SysTables.o SysColumns.o SysIndex.o FreeList.o DataPage.o DBHeader.o dataDirectoryPage.o DataTypes.o DB.o BufferManager*.o main.o -o DBTestDebug.out`
--- trunk/DBEngine/main.cpp
+++ trunk/DBEngine/main.cpp
@@ -1,23 +1,36 @@
 // Main.cpp
 
+#include "DB.h"
+
 void databaseFunction(int choice)
 {
 	string dbName;
+	BufferManager *bu = BufferManager::getBufferManager();
+	int pageSize = (*bu).getPageSize();
+	DB *newdb = new DB();
+	int result,i;
+	query q = (query) malloc(sizeof(querystruct));
 	switch(choice)
 	{
 		case 1: cout<<"\n Enter the name of the database: ";
 			cin>>dbName;
-			createDB(dbName);
+			for(i=0;i<dbName.length();i++)
+				q->dbname[i] = dbName[i];
+			q->dbname[i] = '\0';
+			result = newdb->createDB(q);
 			break;
 		case 10: cout<<"\n Exiting the database";
 			// Call Chinmay's code for flushing the cache....
 			break;
 	}
+
+	free(q);
 }
 
 int main()
 {
 	int choice;
+	fdID = -1;
 
 	do
 	{
@@ -25,7 +38,7 @@ int main()
 		cout<<"\n Enter 10 to exit.";
 		cout<<"\n Enter your choice: ";
 		cin>>choice;
-		if(choice!=1 || choice!=10)
+		if(choice < 1 || choice > 10)
 			continue;
 		else
 			databaseFunction(choice);
