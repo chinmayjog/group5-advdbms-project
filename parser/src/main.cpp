96e887bdf89b208e055aa4bd6aa55cd964e3a22a
jog.chinmay <jog.chinmay@gmail.com>
2013-01-10 11:09:57 +0000

    changed interface for calling DB queries

--- parser/src/main.cpp
+++ parser/src/main.cpp
@@ -7,6 +7,81 @@
 using namespace std;
 
 extern int BufferManagerInterface();
+
+int main( int argc, char *argv[] ) {
+	BufferManager *bu = BufferManager::getBufferManager();
+	(*bu).setPageSize(2048);
+	(*bu).initializeCache(2048); 
+	string s="";
+	DB *curDB = new DB();
+	do{
+		cout<<"\ndb> ";
+		std::getline(cin, s);	
+		if(s=="exit" || s=="exit;") {
+			break;
+		}
+		else if(s=="$cache"){
+			BufferManagerInterface();
+		}
+		else if(s=="$index"){
+			//IndexManagerInterface();
+		}
+		else {
+			string c = s;
+			query q = load_query((const char *)c.c_str());
+			if(q->error!=1){
+				int result;
+				if(s!=""){
+					if(!strcmp(q->type, "CREATEDB")){
+						result = curDB->createDB(q);
+					}
+					else if(!strcmp(q->type, "USEDB")){
+						result = curDB->createDB(q);
+					}
+					else if(!strcmp(q->type,"DROPDB")){
+						result = curDB->dropDB(q);
+					}
+					else if(!strcmp(q->type,"SHOWDB")){
+						result = curDB->showDB(q);
+					}
+					else if(!strcmp(q->type,"SHOWTBL")){
+						//result = curDB->showTables(q);
+					}
+					else if(!strcmp(q->type,"CREATETBL")){
+						//result = curDB->createTable(q);
+					}
+					else if(!strcmp(q->type,"CREATEIND")){
+						//result = curDB->createIndex(q);
+					}
+					else if(!strcmp(q->type,"DROPIND")){
+						//result = curDB->dropIndex(q);
+					}
+					else if(!strcmp(q->type,"INSERT")){
+						//result = curDB->insertEntry(q);
+					}
+					else if(!strcmp(q->type,"DELETE")){
+						//result = curDB->deleteEntry(q);
+					}
+					else if(!strcmp(q->type,"UPDATE")){
+						//result = curDB->updateEntry(q);
+					}
+					else if(!strcmp(q->type,"SELECT")){
+						//result = curDB->selectEntry(q);
+					}
+					else if(!strncmp(q->type,"ALTER",5)){
+					//	result = curDB->selectEntry(q);
+					}	
+					cout<<endl<<"Result is: "<<result<<endl;
+				}
+			}
+			delete(q);
+		}
+	}while(1);
+	delete(curDB);
+    return 0;
+}
+
+
 /*
 void traverse(condition *node){
 	if(node != NULL){
@@ -41,52 +116,3 @@ void traverse(condition *node){
 	}	
 }
 */
-int main( int argc, char *argv[] ) {
-	BufferManager *bu = BufferManager::getBufferManager();
-	(*bu).setPageSize(2048);
-	(*bu).initializeCache(2048); 
-	string s="";
-	do{
-		cout<<"\ndb> ";
-		std::getline(cin, s);	
-		if(s=="exit" || s=="exit;") 
-			break;
-		else if(s=="$cache")
-			BufferManagerInterface();
-		else {
-			string c = s;
-			query q = load_query((const char *)c.c_str());
-			if(q->error!=1){
-				if(s!=""){
-					DB *newdb = new DB();
-					int result = newdb->mainDB(newdb, q);
-					cout<<endl<<"Result is:"<<result<<endl;
-					delete(newdb);
-				}
-					/*if(!strcmp(q->type, "CREATED")){
-						int result = newdb->createDB(q);
-					}
-					else if(!strcmp(q->type, "CREATETBL") || !strcmp(q->type, "CREATEIND"))
-						for(int i = q->cntColumns -1; i>= 0; i--)
-						{
-							cout<<(q->columns[i]).name<<endl;
-							cout<<(q->columns[i]).type<<endl;
-							cout<<(q->columns[i]).function<<endl;
-							cout<<(q->columns[i]).sizeofField1<<endl;
-						}
-					else if(!strcmp(q->type, "INSERT") || !strcmp(q->type, "UPDATE"))
-						for(int i = q->cntColumns -1; i>= 0; i--)
-						{
-							cout<<(q->ins[i]).colname<<endl;
-							cout<<(q->ins[i]).str<<endl;
-						}
-					if(!strcmp(q->type, "SELECT") || !strcmp(q->type, "DELETE")  || !strcmp(q->type, "UPDATE"))
-						//traverse(q->root);
-					cout<<endl; */
-				
-			}
-			delete(q);
-		}
-	}while(1);
-    return 0;
-}
