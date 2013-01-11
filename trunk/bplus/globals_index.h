754575163f06bc5215bf79b719ef0881534a1fdd
noopur.018@gmail.com <noopur.018@gmail.com@8937ff0b-c0a3-5707-dd86-2fc15fcea389>
2013-01-11 21:03:18 +0000

    updated_index

--- trunk/bplus/globals_index.h
+++ trunk/bplus/globals_index.h
@@ -14,15 +14,37 @@
 #include<ctype.h>
 #include<queue>
 
-#define page_size 2048
+#define page_size _pageSize
+/*#ifndef global1
 #define PAGEID_PTR (page_size-sizeof(int))
+#endif
+#ifndef global2
 #define PAGEPRIORITY_PTR (PAGEID_PTR-sizeof(short))
+#endif
+#ifndef global3
 #define NEXT_PTR (PAGEPRIORITY_PTR-sizeof(int))
-#define LEVEL (NEXT_PTR-sizeof(int))
+#endif*/
+#ifndef global4
+#define LEVEL (NEXTPTR-sizeof(int))
+#endif
+#ifndef global5
 #define NUM_NODES (LEVEL-sizeof(int))
+#endif
+#ifndef global6
 #define PAGE_NO (NUM_NODES-sizeof(int))
+#endif
+#ifndef global7
 #define PARENT_NODE (PAGE_NO-sizeof(int))
+#endif
+#ifndef global8
 #define PAGE_PRIORITY (PARENT_NODE-sizeof(int))
+#endif
+#ifndef global9
 #define NEXT (PAGE_PRIORITY-sizeof(int))
+#endif
+#ifndef global10
 #define PREV (NEXT-sizeof(int))
+#endif
+#ifndef global11
 #define PAGEMETADATA (sizeof(int) + sizeof(short) + sizeof(int))
+#endif
