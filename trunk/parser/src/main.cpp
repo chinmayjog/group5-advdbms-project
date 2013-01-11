a8862e9876356b2df38e300a11f194be206a4977
jog.chinmay@gmail.com <jog.chinmay@gmail.com@8937ff0b-c0a3-5707-dd86-2fc15fcea389>
2013-01-11 08:28:50 +0000

    added code for bulk insert from file

--- trunk/parser/src/main.cpp
+++ trunk/parser/src/main.cpp
@@ -1,12 +1,76 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <cstdio>
 #include "parser.h"
 #include "../../DBEngine/DB.h"
 #include "../../cachemgr/src/BufferManager.h"
 using namespace std;
 
 extern int BufferManagerInterface();
+
+void executeQuery(DB *curDB, string s){
+	string c = s;
+	query q = load_query((const char *)c.c_str());
+	if(q->error!=1){
+		int result;
+		if(s!=""){
+			if(!strcmp(q->type, "CREATEDB")){
+				result = curDB->createDB(q);
+			}
+			else if(!strcmp(q->type, "USEDB")){
+				result = curDB->useDB(q);
+			}
+			else if(!strcmp(q->type,"DROPDB")){
+				result = curDB->dropDB(q);
+			}
+			else if(!strcmp(q->type,"SHOWDB")){
+				result = curDB->showDB(q);
+			}
+			else if(!strcmp(q->type,"SHOWTBL")){
+				//result = curDB->showTables(q);
+			}
+			else if(!strcmp(q->type,"CREATETBL")){
+				result = curDB->createTable(q);
+			}
+			else if(!strcmp(q->type,"CREATEIND")){
+				//result = curDB->createIndex(q);
+			}
+			else if(!strcmp(q->type,"DROPIND")){
+				//result = curDB->dropIndex(q);
+			}
+			else if(!strcmp(q->type,"INSERT")){
+				//result = curDB->insertEntry(q);
+			}
+			else if(!strcmp(q->type,"DELETE")){
+				result = curDB->deleteEntry(q);
+			}
+			else if(!strcmp(q->type,"UPDATE")){
+				//result = curDB->updateEntry(q);
+			}
+			else if(!strcmp(q->type,"SELECT")){
+				/*result = curDB->selectEntry(q);
+				if(result > 0){
+					for(int i=0; i<q->totalResultRows; i++){
+						int len = strlen(q->(result[i]));
+						int cntpipes = 0;
+						for(int j=0;j<len;j++){
+							int cntcols = 0;
+							if(result[i][j]=='|'){
+								cntpipes ++;
+							}
+						}
+					}
+				}*/
+			}
+			else if(!strncmp(q->type,"ALTER",5)){
+			//	result = curDB->selectEntry(q);
+			}	
+			cout<<endl<<"Result is: "<<result<<endl;
+		}
+	}
+	delete(q);
+}
 
 int main( int argc, char *argv[] ) {
 	BufferManager *bu = BufferManager::getBufferManager();
@@ -26,73 +90,28 @@ int main( int argc, char *argv[] ) {
 		else if(s=="$index"){
 			//IndexManagerInterface();
 		}
-		else {
-			string c = s;
-			query q = load_query((const char *)c.c_str());
-			if(q->error!=1){
-				int result;
-				if(s!=""){
-					if(!strcmp(q->type, "CREATEDB")){
-						result = curDB->createDB(q);
-					}
-					else if(!strcmp(q->type, "USEDB")){
-						result = curDB->useDB(q);
-					}
-					else if(!strcmp(q->type,"DROPDB")){
-						result = curDB->dropDB(q);
-					}
-					else if(!strcmp(q->type,"SHOWDB")){
-						result = curDB->showDB(q);
-					}
-					else if(!strcmp(q->type,"SHOWTBL")){
-						//result = curDB->showTables(q);
-					}
-					else if(!strcmp(q->type,"CREATETBL")){
-						result = curDB->createTable(q);
-					}
-					else if(!strcmp(q->type,"CREATEIND")){
-						//result = curDB->createIndex(q);
-					}
-					else if(!strcmp(q->type,"DROPIND")){
-						//result = curDB->dropIndex(q);
-					}
-					else if(!strcmp(q->type,"INSERT")){
-						//result = curDB->insertEntry(q);
-					}
-					else if(!strcmp(q->type,"DELETE")){
-						result = curDB->deleteEntry(q);
-					}
-					else if(!strcmp(q->type,"UPDATE")){
-						//result = curDB->updateEntry(q);
-					}
-					else if(!strcmp(q->type,"SELECT")){
-						/*result = curDB->selectEntry(q);
-						if(result > 0){
-							for(int i=0; i<q->totalResultRows; i++){
-								int len = strlen(q->(result[i]));
-								int cntpipes = 0;
-								for(int j=0;j<len;j++){
-									int cntcols = 0;
-									if(result[i][j]=='|'){
-										cntpipes ++;
-									}
-								}
-							}
-						}*/
-					}
-					else if(!strncmp(q->type,"ALTER",5)){
-					//	result = curDB->selectEntry(q);
-					}	
-					cout<<endl<<"Result is: "<<result<<endl;
+		else if(s=="$bulkinsert"){
+			FILE *fp;
+			fp = fopen("insert.sql", "r");
+			if(fp == NULL){
+				cout<<endl<<"File not found"<<endl;
+			} 
+			else {
+				char line[200];
+				while(fgets(line, 199, fp)!= NULL){
+					s = string(line);
+					executeQuery(curDB, s);
 				}
 			}
-			delete(q);
+		}
+		else{
+			executeQuery(curDB, s);
 		}
 	}while(1);
 	delete(curDB);
     return 0;
+	
 }
-
 
 /*
 void traverse(condition *node){
